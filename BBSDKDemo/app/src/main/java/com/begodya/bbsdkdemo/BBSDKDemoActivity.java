c298b635da78486ab2051396b8ddb7de82b777c2
hmily-xinxiang@163.com <hmily-xinxiang@163.com>
2016-06-17 12:28:28 +0800

    Bug fixed

--- BBSDKDemo/app/src/main/java/com/begodya/bbsdkdemo/BBSDKDemoActivity.java
+++ BBSDKDemo/app/src/main/java/com/begodya/bbsdkdemo/BBSDKDemoActivity.java
@@ -1,9 +1,13 @@
 package com.begodya.bbsdkdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.begodya.bbsdkframework.BBSDKFramework;
+import com.begodya.bbsdkframework.widget.SDKH5Activity;
 
 public class BBSDKDemoActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,10 +24,10 @@ public class BBSDKDemoActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.start_button: {
-//                Intent intent = new Intent(this, PierBaseActivity.class);
+//                Intent intent = new Intent(this, SDKH5Activity.class);
 //                startActivity(intent);
 
-//                BBSDKFramework.startPierPayAction(this);
+                BBSDKFramework.startBBSDKAction(this);
                 break;
             }
         }
