7a6dd5e5cd415f8ce4de77934502aa322d5e636d
hmily-xinxiang@163.com <hmily-xinxiang@163.com>
2016-06-17 14:08:52 +0800

    permission

--- BBSDKDemo/app/src/main/java/com/begodya/bbsdkdemo/BBSDKDemoActivity.java
+++ BBSDKDemo/app/src/main/java/com/begodya/bbsdkdemo/BBSDKDemoActivity.java
@@ -1,13 +1,11 @@
 package com.begodya.bbsdkdemo;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.begodya.bbsdkframework.BBSDKFramework;
-import com.begodya.bbsdkframework.widget.SDKH5Activity;
 
 public class BBSDKDemoActivity extends AppCompatActivity implements View.OnClickListener {
 
