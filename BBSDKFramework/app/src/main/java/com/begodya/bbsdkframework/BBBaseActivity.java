17b08a4cb6c0c94f863d5e045fb7f8078f12afaf
hmily-xinxiang@163.com <hmily-xinxiang@163.com>
2016-06-17 13:31:44 +0800

    sdk framework

--- BBSDKFramework/app/src/main/java/com/begodya/bbsdkframework/BBBaseActivity.java
+++ BBSDKFramework/app/src/main/java/com/begodya/bbsdkframework/BBBaseActivity.java
@@ -3,13 +3,8 @@ package com.begodya.bbsdkframework;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
